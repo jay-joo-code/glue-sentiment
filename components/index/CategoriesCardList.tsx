1e0bc82b481520b39fbd2c35b967ddee1cdf7252
Jay Joo <jj534@cornell.edu>
2022-07-31 20:42:17 -0400

    Init category section

--- components/index/CategoriesCardList.tsx
+++ components/index/CategoriesCardList.tsx
@@ -2,14 +2,17 @@ import { Container, Header, Text, Title } from "@mantine/core"
 import Flex from "components/glue/Flex"
 import React from "react"
 import CategoryCard from "./CategoryCard"
+import ChairIcon from "@mui/icons-material/Chair"
 
 const CategoriesCardList = () => {
   return (
     <Container my="3rem">
-      <Title order={2}>Categories</Title>
+      <Title order={2} mb="md">
+        Categories
+      </Title>
       <Flex align="center" direction="row">
-        <CategoryCard name="Dorm" />
-        <CategoryCard name="Course" />
+        <CategoryCard name="Dorm" svg={<ChairIcon />} />
+        <CategoryCard name="Course" svg={<ChairIcon />} />
       </Flex>
     </Container>
   )
--- components/index/CategoryCard.tsx
+++ components/index/CategoryCard.tsx
@@ -3,18 +3,34 @@ import React from "react"
 
 interface ICategoryCardProps {
   name: string
+  svg: React.ReactNode
 }
 
-const CategoryCard = ({ name }: ICategoryCardProps) => {
+const CategoryCard = ({ name, svg }: ICategoryCardProps) => {
   return (
     <Container
+      px="md"
+      py="sm"
       sx={(theme) => ({
         background: theme.colors.gray[1],
         borderRadius: "6px",
         width: "44%",
+        height: "120px",
+        position: "relative",
       })}
     >
-      <Text>{name}</Text>
+      <Text size="lg" weight={500}>
+        {name}
+      </Text>
+      <Container
+        sx={(theme) => ({
+          position: "absolute",
+          bottom: "10px",
+          right: "10px",
+        })}
+      >
+        {svg}
+      </Container>
     </Container>
   )
 }
