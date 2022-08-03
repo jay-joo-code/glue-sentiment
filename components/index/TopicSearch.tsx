4dc22075611705b492eb1d0cf669c136d41b856c
Jay Joo <jj534@cornell.edu>
2022-08-02 21:44:06 -0400

    fix search section UI

--- components/index/TopicSearch.tsx
+++ components/index/TopicSearch.tsx
@@ -1,36 +1,16 @@
-import { Input, Text } from "@mantine/core"
+import { Container, Input, Text, Title } from "@mantine/core"
 import Flex from "components/glue/Flex"
 import React from "react"
 import SearchIcon from "@mui/icons-material/Search"
 
 const TopicSearch = () => {
   return (
-    <Flex direction="column" align="center">
-      <Text
-        variant="gradient"
-        gradient={{ from: "indigo", to: "cyan", deg: 45 }}
-        weight={500}
-        sx={(theme) => ({
-          fontSize: "3rem",
-          letterSpacing: "2px",
-        })}
-      >
-        Sentiment
-      </Text>
-      <Input
-        radius="xl"
-        iconWidth={44}
-        icon={<SearchIcon />}
-        sx={(theme) => ({
-          width: "100%",
-          maxWidth: "400px",
-
-          "& svg": {
-            marginLeft: "1rem",
-          },
-        })}
-      />
-    </Flex>
+    <Container>
+      <Title order={2} mb="md">
+        Search
+      </Title>
+      <Input radius="xl" icon={<SearchIcon />} />
+    </Container>
   )
 }
 
