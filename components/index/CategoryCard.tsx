83e858191269b865a9efc92710a05a928621ad25
Jay Joo <jj534@cornell.edu>
2022-08-06 01:08:59 -0400

    conditionally make card clickable

--- components/index/CategoryCard.tsx
+++ components/index/CategoryCard.tsx
@@ -13,56 +13,58 @@ interface ICategoryCardProps {
 }
 
 const CategoryCard = ({ category }: ICategoryCardProps) => {
-  return (
-    <Link href={`/category/${category?.id}`}>
-      <Container
-        p="xs"
-        sx={(theme) => ({
-          borderRadius: "6px",
-          cursor: "pointer",
+  const svgFileName = pluralize(category?.name)
+  const svgPath = `/category-icons/${svgFileName.replace(" ", "-")}.svg`
 
-          [`@media (min-width: ${theme.breakpoints.xs}px)`]: {
-            "&:hover": {
-              background: theme.colors.gray[0],
-            },
+  const renderComponent = () => (
+    <Container
+      p="xs"
+      sx={(theme) => ({
+        borderRadius: "6px",
+        cursor: "pointer",
+
+        [`@media (min-width: ${theme.breakpoints.xs}px)`]: {
+          "&:hover": {
+            background: theme.colors.gray[0],
           },
+        },
+      })}
+    >
+      <Flex
+        align="center"
+        justify="center"
+        sx={(theme) => ({
+          background: theme.colors.gray[1],
+          borderRadius: "8px",
+          width: "10rem",
+          height: "14rem",
         })}
+        mb=".4rem"
       >
-        <Flex
-          align="center"
-          justify="center"
-          sx={(theme) => ({
-            background: theme.colors.gray[1],
-            borderRadius: "8px",
-            width: "10rem",
-            height: "14rem",
-          })}
-          mb=".4rem"
-        >
-          <div>
-            <Image
-              src={`/category-icons/${pluralize(category?.name)}.svg`}
-              alt=""
-              height={80}
-              width={80}
-            />
-          </div>
-        </Flex>
-        <Text mb=".2rem" ml=".2rem" weight={600} size="md">
-          {capitalize(pluralize(category?.name))}
+        <div>
+          <Image src={svgPath} alt="" height={80} width={80} />
+        </div>
+      </Flex>
+      <Text mb=".2rem" ml=".2rem" weight={600} size="md">
+        {capitalize(pluralize(category?.name))}
+      </Text>
+      {category?.isComingSoon ? (
+        <Badge radius="sm" size="xs">
+          Coming soon
+        </Badge>
+      ) : (
+        <Text size="xs" ml=".2rem" weight={500} color="dimmed">
+          0 {pluralize(category?.name)}
         </Text>
-        {category?.isComingSoon ? (
-          <Badge radius="sm" size="xs">
-            Coming soon
-          </Badge>
-        ) : (
-          <Text size="xs" ml=".2rem" weight={500} color="dimmed">
-            0 {pluralize(category?.name)}
-          </Text>
-        )}
-      </Container>
-    </Link>
+      )}
+    </Container>
   )
+
+  if (category?.isComingSoon) {
+    return renderComponent()
+  }
+
+  return <Link href={`/category/${category?.id}`}>{renderComponent()}</Link>
 }
 
 export default CategoryCard
