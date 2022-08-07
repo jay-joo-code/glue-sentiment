4b035d6c69d9c3d3241be84e15a644383c84ec47
Jay Joo <jj534@cornell.edu>
2022-08-07 16:55:50 -0400

    fetch searched topics

--- components/index/TopicSearch.tsx
+++ components/index/TopicSearch.tsx
@@ -1,15 +1,47 @@
 import { Container, Input, Text, Title } from "@mantine/core"
 import Flex from "components/glue/Flex"
-import React from "react"
+import React, { useState } from "react"
 import SearchIcon from "@mui/icons-material/Search"
+import useSWR from "swr"
+import { useDebouncedValue } from "@mantine/hooks"
 
 const TopicSearch = () => {
+  const [searchQuery, setSearchQuery] = useState<string>("")
+  const [debouncedSearchQuery] = useDebouncedValue(searchQuery, 500)
+  const { data: topics } = useSWR(
+    debouncedSearchQuery
+      ? [
+          "/glue/topics",
+          {
+            where: {
+              name: {
+                contains: debouncedSearchQuery,
+              },
+            },
+            page: 0,
+            limit: 5,
+            parseConfig: {
+              parseNumbers: false,
+            },
+          },
+        ]
+      : null
+  )
+
+  console.log("topics", topics)
+
   return (
     <Container>
       <Title order={2} mb="md">
         Search
       </Title>
-      <Input radius="xl" icon={<SearchIcon />} />
+      <Input
+        radius="xl"
+        icon={<SearchIcon />}
+        value={searchQuery}
+        onChange={(event) => setSearchQuery(event?.target?.value)}
+      />
+      {}
     </Container>
   )
 }
