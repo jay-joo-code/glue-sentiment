78c89ef175c88f6731be39afa4e92a0178197fc1
Jay Joo <jj534@cornell.edu>
2022-08-07 16:55:39 -0400

    scrape synchronously

--- util/scrapeRoster.ts
+++ util/scrapeRoster.ts
@@ -104,43 +104,44 @@ export const fetchAllClasses = (): Promise<boolean> =>
                 const classes = await fetchAllClassesBySubject(value)
 
                 // NOTE: synchronus save
-                // classes?.forEach((classData) => allClasses.push(classData))
+                classes?.forEach((classData) => allClasses.push(classData))
 
-                const savePromises = classes?.map(
-                  async (classObj, classIdx) => {
-                    const classData = {
-                      name: `${classObj?.subject} ${classObj?.catalogNbr}`,
-                      subtitle: classObj?.titleLong,
-                      desc: classObj?.description,
-                      categoryId: courseCategory?.id,
-                      provider: "roster",
-                      providerId: String(classObj?.crseId),
-                    }
-                    setTimeout(async () => {
-                      await prisma.topic.upsert({
-                        create: classData,
-                        update: classData,
-                        where: {
-                          providerId: String(classObj?.crseId),
-                        },
-                      })
-                      console.log(
-                        "saved",
-                        `${classObj?.subject} ${classObj?.catalogNbr}`
-                      )
+                // NOTE: async save
+                // const savePromises = classes?.map(
+                //   async (classObj, classIdx) => {
+                //     const classData = {
+                //       name: `${classObj?.subject} ${classObj?.catalogNbr}`,
+                //       subtitle: classObj?.titleLong,
+                //       desc: classObj?.description,
+                //       categoryId: courseCategory?.id,
+                //       provider: "roster",
+                //       providerId: String(classObj?.crseId),
+                //     }
+                //     setTimeout(async () => {
+                //       await prisma.topic.upsert({
+                //         create: classData,
+                //         update: classData,
+                //         where: {
+                //           providerId: String(classObj?.crseId),
+                //         },
+                //       })
+                //       console.log(
+                //         "saved",
+                //         `${classObj?.subject} ${classObj?.catalogNbr}`
+                //       )
 
-                      // NOTE: synchronus save
-                      // console.log(
-                      //   "saved",
-                      //   `${classObj?.subject} ${classObj?.catalogNbr}`,
-                      //   (classIdx / allClasses?.length) * 100,
-                      //   "% saved"
-                      // )
-                    }, 100 * classIdx)
-                  }
-                )
+                //       // NOTE: synchronus save
+                //       // console.log(
+                //       //   "saved",
+                //       //   `${classObj?.subject} ${classObj?.catalogNbr}`,
+                //       //   (classIdx / allClasses?.length) * 100,
+                //       //   "% saved"
+                //       // )
+                //     }, 100 * classIdx)
+                //   }
+                // )
 
-                await Promise.all(savePromises)
+                // await Promise.all(savePromises)
 
                 console.log(`Fetched ${classes.length} ${value} courses`)
                 resolve()
@@ -151,32 +152,32 @@ export const fetchAllClasses = (): Promise<boolean> =>
       await Promise.all(promises)
 
       // NOTE: synchronus save
-      // const savePromises = allClasses?.map(async (classObj, classIdx) => {
-      //   const classData = {
-      //     name: `${classObj?.subject} ${classObj?.catalogNbr}`,
-      //     subtitle: classObj?.titleLong,
-      //     desc: classObj?.description,
-      //     categoryId: courseCategory?.id,
-      //     provider: "roster",
-      //     providerId: String(classObj?.crseId),
-      //   }
-      //   setTimeout(async () => {
-      //     await prisma.topic.upsert({
-      //       create: classData,
-      //       update: classData,
-      //       where: {
-      //         providerId: String(classObj?.crseId),
-      //       },
-      //     })
-      //     console.log(
-      //       "saved",
-      //       `${classObj?.subject} ${classObj?.catalogNbr}`,
-      //       (classIdx / allClasses?.length) * 100,
-      //       "% saved"
-      //     )
-      //   }, 100 * classIdx)
-      // })
-      // await Promise.all(savePromises)
+      const savePromises = allClasses?.map(async (classObj, classIdx) => {
+        const classData = {
+          name: `${classObj?.subject} ${classObj?.catalogNbr}`,
+          subtitle: classObj?.titleLong,
+          desc: classObj?.description,
+          categoryId: courseCategory?.id,
+          provider: "roster",
+          providerId: String(classObj?.crseId),
+        }
+        setTimeout(async () => {
+          await prisma.topic.upsert({
+            create: classData,
+            update: classData,
+            where: {
+              providerId: String(classObj?.crseId),
+            },
+          })
+          console.log(
+            "saved",
+            `${classObj?.subject} ${classObj?.catalogNbr}`,
+            (classIdx / allClasses?.length) * 100,
+            "% saved"
+          )
+        }, 100 * classIdx)
+      })
+      await Promise.all(savePromises)
       resolve(true)
     })()
   })
