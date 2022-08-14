4a7bed49dc4f097d0571297e359387a23d7a9615
Jay Joo <jj534@cornell.edu>
2022-08-14 11:56:08 -0400

    add invalidate admin page

---
+++ pages/admin/invalidate.tsx
@@ -0,0 +1,66 @@
+import { Stack } from "@mantine/core"
+import GlueInfiniteScroll from "components/glue/GlueInfiniteScroll"
+import PageContainer from "components/glue/PageContainer"
+import ReviewItem from "components/review/ReviewItem"
+import { useSession } from "next-auth/react"
+import React from "react"
+
+const AdminInvalidatePage = () => {
+  const { data: session } = useSession()
+
+  if (session?.user?.email !== "cornellsentiment@gmail.com") {
+    return <div>unauthorized</div>
+  }
+
+  return (
+    <PageContainer
+      title="Invalid reviews"
+      variant="mobile-only"
+      isPrivate={true}
+    >
+      <GlueInfiniteScroll
+        queryConfig={{
+          url: "/glue/reviews",
+          args: {
+            where: {
+              invalidVotes: {
+                gte: 2,
+              },
+              isValid: true,
+            },
+            include: {
+              topic: {
+                include: {
+                  category: true,
+                },
+              },
+            },
+          },
+        }}
+        limit={5}
+      >
+        {(providedData) => {
+          const { data } = providedData
+
+          if (data?.length === 0) {
+            return <div>no data</div>
+          }
+          return (
+            <Stack>
+              {data?.map((review) => (
+                <ReviewItem
+                  key={review?.id}
+                  review={review}
+                  onUpvoteToggle={() => {}}
+                  renderTopic={true}
+                />
+              ))}
+            </Stack>
+          )
+        }}
+      </GlueInfiniteScroll>
+    </PageContainer>
+  )
+}
+
+export default AdminInvalidatePage
