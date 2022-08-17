f762ff1712867aa4bf658a5a012adc1718f6c308
Jay Joo <jj534@cornell.edu>
2022-08-17 13:24:23 -0400

    handle no reviews

--- util/getAverageReviewStars.ts
+++ util/getAverageReviewStars.ts
@@ -1,8 +1,9 @@
 import { Review } from "@prisma/client"
 
 const getAverageReviewStars = (reviews: Review[]) => {
+  if (reviews?.length === 0) return 0
   const stars = reviews?.map((review) => review?.stars)
-  const average = stars.reduce((a, b) => a + b) / stars.length
+  const average = stars.reduce((a, b) => a + b, 0) / stars.length
   return average
 }
 
