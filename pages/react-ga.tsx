479409249f8d07fd4373ec78161b25aa0b76ff5e
Jay Joo <jj534@cornell.edu>
2022-09-05 16:47:42 -0400

    add ga test page

---
+++ pages/react-ga.tsx
@@ -0,0 +1,13 @@
+import GlueButton from "components/glue/GlueButton"
+import PageContainer from "components/glue/PageContainer"
+import React from "react"
+
+const ReactGAPage = () => {
+  return (
+    <PageContainer variant="mobile-only" title="GA test page title">
+      <GlueButton>test button</GlueButton>
+    </PageContainer>
+  )
+}
+
+export default ReactGAPage
