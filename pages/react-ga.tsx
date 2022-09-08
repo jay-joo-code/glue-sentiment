d972bd48955abb56878f55033033b84d2d5d3c28
Jay Joo <jj534@cornell.edu>
2022-09-08 16:43:40 -0400

    add two more test buttons

--- pages/react-ga.tsx
+++ pages/react-ga.tsx
@@ -1,3 +1,4 @@
+import { Stack } from "@mantine/core"
 import GlueButton from "components/glue/GlueButton"
 import PageContainer from "components/glue/PageContainer"
 import React from "react"
@@ -5,7 +6,11 @@ import React from "react"
 const ReactGAPage = () => {
   return (
     <PageContainer variant="mobile-only" title="GA test page title">
-      <GlueButton>test button</GlueButton>
+      <Stack>
+        <GlueButton>test button A</GlueButton>
+        <GlueButton>test button B</GlueButton>
+        <GlueButton>test button C</GlueButton>
+      </Stack>
     </PageContainer>
   )
 }
